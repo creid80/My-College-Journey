d98a85a0120a7aa6077c011271977af5e9a8d393
Carol Reid <creid80@wgu.edu>
2023-05-23 14:16:05 -0400

    second commit

---
+++ app/src/main/java/com/example/c196carolreid/UI/PartDetails.java
@@ -0,0 +1,16 @@
+package com.example.c196carolreid.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.c196carolreid.R;
+
+public class PartDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part_details);
+    }
+}
--- app/src/main/java/com/example/c196carolreid/UI/ProductDetails.java
+++ app/src/main/java/com/example/c196carolreid/UI/ProductDetails.java
@@ -5,22 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.c196carolreid.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class ProductList extends AppCompatActivity {
+public class ProductDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_list);
-        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        setContentView(R.layout.activity_product_details);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ProductList.this, ProductDetails.class);
+                Intent intent = new Intent(ProductDetails.this, PartDetails.class);
                 startActivity(intent);
             }
         });
